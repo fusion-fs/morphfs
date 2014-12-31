9b7214b1266bac6e43975c5a4b76bdc0aad7dba3
Huamin Chen <fusion.filesystem@gmail.com>
2014-12-31 17:27:00 -0500

    init deposit
    Signed-off-by: Huamin Chen <fusion.filesystem@gmail.com>

---
+++ pre.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npm install -g --unsafe-perm fuse4js hiredis redis thrift
---
+++ src/thrift/rpc.thrift
@@ -0,0 +1,62 @@
+enum Capability
+{
+	LIST = 1,
+	READ = 2,
+	WRITE = 4,
+	DELETE = 8,
+}
+
+enum Status
+{
+	SUCCESS = 0,
+	ERROR = 1,
+}
+
+struct ShowArg
+{
+	1:string key,
+	2:i64    offset,
+        3:i64    len,
+}
+
+struct ShowRes
+{
+	1:Status   status,
+	2:optional i64 len,
+        3:optional list<string> data,
+}
+
+struct ReadArg
+{
+	1:string key,
+	2:i64    offset,
+        3:i64    len,
+}
+
+struct ReadRes
+{
+	1:Status   status,
+	2:optional i64 len,
+        3:optional string data,
+}
+
+struct WriteArg
+{
+	1:string key,
+	2:i64    offset,
+        3:i64    len,
+	4:string data,
+}
+
+struct WriteRes
+{
+	1:Status   status,
+	2:optional i64 len,
+}
+
+service RPC 
+{
+  ShowRes show (1:ShowArg arg),
+  ReadRes read (1:ReadArg arg),
+  WriteRes write (1:WriteArg arg),
+}
